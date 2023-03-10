f5a6da542d901687eeb125188a11cb60eac053fa
StepanMelikyan <98053309+StepanMelikyan@users.noreply.github.com>
2023-03-10 18:59:19 +0300

    Add files via upload

---
+++ toys/toys/src/Toy.java
@@ -0,0 +1,15 @@
+public class Toy {
+    public int _ID;
+    public String _name;
+    public int _Quantity;
+    public int _weight;
+
+    public Toy (int ID, String name, int Quantity, int weight) {
+        _ID = ID;
+        _name = name;
+        _Quantity = Quantity;
+        _weight = weight;
+
+    }
+
+}
